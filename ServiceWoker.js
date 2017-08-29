fb1657b59e68365084873fef9a5cf81153741a1f
caoys <caoys@xiaoshouyi.com>
2017-08-29 17:57:24 +0800

    初始化Service Worker文件

---
+++ ServiceWoker.js
@@ -0,0 +1,41 @@
+﻿var CACHE_NAME = "offline_test_v1";
+
+self.addEventListener("install", function(event){
+	console.log("install");
+	/*event.waitUntil(
+		caches.open(CACHE_NAME);
+	);*/
+});
+
+self.addEventListener("activate", function(event){
+	console.log("activate");
+	event.waitUntil(
+		return new Promise.all(
+			caches.keys().map(function(cacheName){
+				if(CACHE_NAME !== cacheName && cacheName.startsWith("offline_test")) {
+					return caches.delete(cacheName);
+				}
+			});
+		);
+	);
+});
+
+self.addEventListener("fetch",function(event){
+	console.log(event.request.url);
+	evnet.responseWith(
+		fetch(event.request).then(function(res){
+			caches.open(CACHE_NAME).then(function(cache){
+				cache.put(event.request,res);
+			});
+			return res.clone();
+		}).catch(function(){
+			return caches.match(event.request).then(function(res){
+				if(res){
+					return res;
+				}else{
+					console.error("No Resource for : " + event.request.url);
+				}
+			})
+		})
+	);
+});
